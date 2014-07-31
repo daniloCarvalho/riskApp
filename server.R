5d746a04d5febd716879593b482b54fef56b93a6
Danilo Carvalho <danilo_carvalho@edu.unifor.br>
2014-07-31 09:02:57 -0300

    Fix loading data method

--- server.R
+++ server.R
@@ -1,6 +1,22 @@
 library(shiny)
 
-coefficientsRef <- read.csv("~/GitHub/riskApp/coefficientsRef.csv")
+coefficientsRef <- data.frame(gender = c("female","female","female","male","male","male"),
+                              race = c("white","black","other","white","black","other"),
+                              age = c(-29.799,17.114,-29.799,12.344,2.469,12.344),
+                              age2 = c(4.884,NA,4.884,NA,NA,NA),
+                              totalCholesterol = c(13.54,0.94,13.54,11.853,0.302,11.853),
+                              totalCholesterolAge = c(-3.114,NA,-3.114,-2.664,NA,-2.664),
+                              hdl = c(-13.578,-18.92,-13.578,-7.99,-0.307,-7.99),
+                              hdlAge = c(3.149,4.475,3.149,1.769,NA,1.769),
+                              treatSysBP = c(2.019,29.291,2.019,1.797,1.916,1.797),
+                              treatSysBPAge = c(NA,-6.432,NA,NA,NA,NA),
+                              NotreatSysBP = c(1.957,27.82,1.957,1.764,1.809,1.764),
+                              NotreatSysBPAge = c(NA,-6.087,NA,NA,NA,NA),
+                              smoker = c(7.574,0.691,7.574,7.837,0.549,7.837),
+                              smokerAge = c(-1.665,NA,-1.665,-1.795,NA,-1.795),
+                              diabetes = c(0.661,0.874,0.661,0.658,0.645,0.658),
+                              baseMean = c(-29.18,86.61,-29.18,61.18,19.54,61.18),
+                              baseSurv = c(0.9665,0.9533,0.9665,0.9144,0.8854,0.9144))
 
 shinyServer(function(input,output) {
 
